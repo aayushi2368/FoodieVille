0805e1ac121641425ff25fd1c9279c56b2bd346a
aayushi2368 <153229780+aayushi2368@users.noreply.github.com>
2024-12-16 16:45:32 +0530

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
       <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <svg className="bi" width="30" height="24"></svg>
       </Link>
-      <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+      <span className="mb-3 mb-md-0 text-muted">© 2024 Company, GoFood</span>
     </div>
 
    
