5e431edff719346d0f7190da93057c7240f4547f
aayushi2368 <153229780+aayushi2368@users.noreply.github.com>
2025-01-02 23:31:56 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Card from '../components/Card'
+//import Login from '../screen/Login'
+// import Register from '../screen/Register'
+// import Footer from '../components/Footer'
+
+
+export default function Home() {
+  return (
+     <div>
+      <div> <Navbar/> </div>
+    <div> <Card/> </div>
+     {/* <div> <Login/> </div>  */}
+     {/* <div><Register/></div> */}
+    </div>
+  )
+}
+      
